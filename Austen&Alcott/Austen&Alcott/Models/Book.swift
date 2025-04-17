ec93972f3f708b9f4fa5cfd592448852b3280258
CrhistianNinalaya <crhistianninalaya@gmail.com>
2025-04-17 02:10:50 -0500

    feat: Add Book Class

---
+++ Austen&Alcott/Austen&Alcott/Models/Book.swift
@@ -0,0 +1,18 @@
+//
+//  Book.swift
+//  Austen&Alcott
+//
+//  Created by Crhistian Ninalaya on 17/04/25.
+//
+
+import Foundation
+
+class Book {
+    var title: String
+    var author: String
+    
+    init(title: String, author: String) {
+        self.title = title
+        self.author = author
+    }
+}
